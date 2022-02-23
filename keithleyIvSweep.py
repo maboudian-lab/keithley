cf71bf55f5a17978d08a93cea64bfa858b8fabb8
maboudianlab <93733429+maboudianlab@users.noreply.github.com>
2022-02-22 16:05:13 -0800

    Fixed issues with resource manager being unable to detect Keithley over USB

--- keithleyIvSweep.py
+++ keithleyIvSweep.py
@@ -11,6 +11,8 @@ Rev: 22 February 2022
 Documentation:
     https://keithley2600.readthedocs.io/en/latest/api/keithley_driver.html#keithley_driver.Keithley2600.voltage_sweep_single_smu
 Also requires pyusb module!
+On Windows, install NI-VISA and use IVI backend instead of pyvisa
+VISA library: C:\Windows\System32\visa64.dll
 """
 
 from keithley2600 import Keithley2600, ResultTable
@@ -18,14 +20,16 @@ import pyvisa
 import numpy as np
 import csv
 
-# using pyvisa-py as VISA backend, list connected instruments
-# rm = pyvisa.ResourceManager('@py')
-rm = pyvisa.ResourceManager('@py')
-print(rm.list_resources())
+# auto-detect IVI backend and fallback to pyvisa '@py' otherwise
+rm = pyvisa.ResourceManager()
+rmList = rm.list_resources()
+print(rmList)
 
-def keithleyIV(fileName, vMin, vMax, vStep=0.1, tInt=0.12, delay=-1, usbAddr='USB0::1510::9730::4062092\x00::0::INSTR'):
+# default for usbAddr is first entry of rmList
+def keithleyIV(fileName, vMin, vMax, vStep=0.1, tInt=0.12, delay=-1, usbAddr=rmList[0]):
     # initialize keithley object
-    k = Keithley2600('usbAddr')
+    # visa_library defaults to pyvisa unless empty string '' is passed!
+    k = Keithley2600(usbAddr,visa_library='')
     
     # voltage sweep from vmin to vmax with step size vstep
     # delay=-1 means no delay
