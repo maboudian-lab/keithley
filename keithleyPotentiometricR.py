5821a6e0456bb2380dbba9d844f94914344459d8
maboudianlab <93733429+maboudianlab@users.noreply.github.com>
2022-07-20 18:52:52 -0700

    Using voltage_sweep_dual_smu for I measurements

--- keithleyPotentiometricR.py
+++ keithleyPotentiometricR.py
@@ -4,6 +4,9 @@ import pyvisa
 import numpy as np
 import csv
 
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 # auto-detect IVI backend and fallback to pyvisa '@py' otherwise
 rm = pyvisa.ResourceManager()
 rmList = rm.list_resources()
@@ -12,22 +15,39 @@ print(rmList)
 usbAddr=rmList[0]
 
 k = Keithley2600(usbAddr,visa_library='')
-
-# create ResultTable with two columns
-rt = ResultTable(
-    column_titles=['Voltage', 'Current'],
-    units=['V', 'A'],
-    params={'recorded': time.asctime(), 'sweep_type': 'iv'},
-)
-
-# create live plot which updates as data is added
-rt.plot(live=True)
-
-# measure some currents
 v = 5
-k.apply_voltage(k.smua, v)
-i = k.smua.measure.i()
-rt.append_row([v, i])
+tEnd = 3
+channel = "both"
 
-# save the data
-rt.save('iv_curve.txt')
+tInt = 0.12
+delay = -1
+
+t0 = time.time()
+dt = 0
+
+fileName = "test.csv"
+
+
+with open(fileName, 'a', newline='') as csvFile:
+    while dt <= tEnd:
+        # voltage sweep from vmin to vmax with step size vstep
+        # delay=-1 means no delay
+        sweepList = [v]
+        data = k.voltage_sweep_dual_smu(
+            k.smua, k.smub, sweepList, sweepList, t_int=tInt, delay=delay, pulsed=False
+        )
+        t = time.time()
+        dt = t - t0
+        
+        data = np.transpose(np.array(data))
+        np.insert(data,1,t)
+        
+        writer = csv.writer(csvFile)
+        writer.writerows(data)
+    
+    # print(dt)
+    # print(data)
+
+k.reset()
+
+# print(dt)
